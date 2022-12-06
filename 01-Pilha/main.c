02728ffbea73671685d94d20a0e53ad89aab863e
clark25 <jonatas_s.v.s@hotmail.com>
2022-12-06 08:32:32 -0300

    inicio construcao pilha

---
+++ 01-Pilha/main.c
@@ -0,0 +1,50 @@
+// Implementando uma pilha estática usando vetor
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+// N  ira determinar a capacidade da pilha estática
+#define N 1000
+
+// Inicializa o top da pilha sendo -1
+int top = -1;
+
+// Inicializa a pilha usando vetor
+int stack[N];
+
+
+// Escopo das funções
+void push();
+int pop();
+int peek();
+bool isEmpty();
+bool isFull();
+
+
+int main(){
+  printf("Vetor Estatico (Capacidade Total: %d)\n", N);
+  int choice;
+
+  while(1){
+    system("clear");
+    printf("\nEscolha qualquer uma das opcoes a seguir:\n");
+    printf(" 0: Sair\n");
+    printf(" 1: Push\n");
+    printf(" 2: Pop\n");
+    printf(" 3: Peek\n");
+    printf(" 4: Verifica se a pilha esta vazia\n");
+    printf(" 5: Verifica se a pilha esta cheia\n");
+    scanf("%d", &choice);
+
+    switch (choice){
+      case 0:
+        exit(0);
+        break;
+      
+      default:
+        printf("Por favor escolha uma opcao valida!\n");
+    }
+  }
+  
+  return 0;
+}
